9ef74359c2b0bde75abdfba48efc4dcf30e069d6
INPRRAJ11 <prasanth.raj@in.abb.com>
2022-01-15 19:32:32 +0530

    file cpoy

---
+++ FileCopy.bat
@@ -0,0 +1,4 @@
+@echo off
+cd
+copy "D:\Auto SSH Download_Smoke_Automation\From TFS Downloaded\Inputs.xls" D:\TAPCM600Automation\TAPCM600\Inputs
+copy "D:\Auto SSH Download_Smoke_Automation\From TFS Downloaded\config.xml" "D:\Auto SSH Download_Smoke_Automation\SSH Download"
